c296574c526d8e0215f21a1aa374918b25bc8b06
Pedro Ruiz <peruvapedro99@gmail.com>
2015-08-30 19:25:29 +0200

    Opcion de movimiento implementadas

--- coche/coche.ino
+++ coche/coche.ino
@@ -6,24 +6,21 @@ int leds =  3,
  motorb =  4,   // hacia atras
  direcciona = 7, // Derecha
  direccionb = 8,  // izquierda
- clason =  5,
  sensor = 6; 
- coche ch;
+ Coche coche = Coche(motora,motorb,direcciona,direccionb);
+ 
 void setup()
 {
   pinMode(leds, OUTPUT);
   pinMode(motora, OUTPUT);
   pinMode(motorb, OUTPUT);
-  pinMode(clason, OUTPUT);
   pinMode(sensor, INPUT);
-  pinMode(clason, OUTPUT);
   pinMode(direcciona, OUTPUT);
   pinMode(direccionb, OUTPUT);
-  ch.avance();
 }
 
 //Principal
 void loop()
 {
- 
+
 }
--- libraries/control/control.h
+++ libraries/control/control.h
@@ -2,18 +2,62 @@
 #define control_h
 
 #include "Arduino.h"
-class coche
+
+class Coche
 {
-  public:
-    void ini(int a, int b , int c, int d);
-    void avance(void);
-    void retroceso(void);
-    void freno(void);
-    void derecha(void);
-    void izquierda(void);
-    void recto(void);
-  private:
-    int leds, motora, motorb, direcciona, direccionb, clason,
-sensor;
+public:
+  Coche(int a, int b , int c, int d);
+  void avance(void);
+  void retroceso(void);
+  void freno(void);
+  void derecha(void);
+  void izquierda(void);
+  void recto(void);
+private:
+  int leds, motora, motorb, direcciona, direccionb, sensor;
 };
 #endif
+
+
+Coche::Coche(int ma, int mb, int da, int db)
+{
+ motora = ma;
+ motorb = mb;
+ direcciona = da;
+ direccionb = db;
+}
+
+void Coche::avance(void)
+{
+ digitalWrite(motorb, LOW);  
+ digitalWrite(motora, HIGH);
+}
+
+void Coche::retroceso(void)
+{
+ digitalWrite(motora, LOW);  
+ digitalWrite(motorb, HIGH);
+}
+
+void Coche::freno(void)
+{
+ digitalWrite(motora, LOW);  
+ digitalWrite(motorb, LOW);
+}
+
+void Coche::izquierda(void)
+{
+  digitalWrite(direcciona, LOW);
+  digitalWrite(direccionb, HIGH);
+}
+void Coche::derecha(void)
+{
+ digitalWrite(direccionb, LOW);
+ digitalWrite(direcciona, HIGH); 
+}
+void Coche::recto(void)
+{
+ digitalWrite(direcciona, LOW);
+ digitalWrite(direccionb, LOW); 
+}
+
